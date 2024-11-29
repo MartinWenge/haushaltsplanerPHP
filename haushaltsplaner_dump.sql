6c5eaa82ef60fd8551b0c4a6f58cd6f37af2fd97
MartinWenge <wengenmayr.martin@outlook.de>
2024-11-29 17:47:11 +0100

    add new tasks about car and garbage

--- haushaltsplaner_dump.sql
+++ haushaltsplaner_dump.sql
@@ -28,7 +28,7 @@ CREATE TABLE `accounts` (
   `password` varchar(255) NOT NULL,
   `email` varchar(127) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -58,7 +58,7 @@ CREATE TABLE `aufgaben` (
   `bild` varchar(255) DEFAULT NULL,
   `kategorie` varchar(127) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -67,7 +67,7 @@ CREATE TABLE `aufgaben` (
 
 LOCK TABLES `aufgaben` WRITE;
 /*!40000 ALTER TABLE `aufgaben` DISABLE KEYS */;
-INSERT INTO `aufgaben` VALUES (1,'Wäsche waschen','alle zwei Tage','Wäsche sortieren, Waschmaschine befüllen, Waschmittel rein und starten',10,15,'figures/aufgabe_waescheWaschen_640.jpg','Wäsche'),(2,'Wäsche trocknen','alle zwei Tage','Wäsche aus der Waschmaschine holen und aufhängen oder im Trockner trocknen',20,30,'figures/aufgabe_waescheTrocknen_640.jpg','Wäsche'),(3,'Wäsche legen','alle zwei Tage','trockene Wäsche legen und in den passenden Schrank einräumen',20,30,'figures/aufgabe_waescheLegen_640.jpg','Wäsche'),(4,'Boden wischen','einmal pro Woche','in allen Zimmer, die es nötig haben, den Boden nass wischen',100,45,'figures/aufgabe_bodenWischen_640.jpg','Putzen'),(5,'Wechselsachen und Windeln in der Kita auffüllen','alle zwei Wochen','Nachschauen, ob im Kindergarten noch genügend passende und dem Wetter angepasste Wechselsachen und falls nötig Windeln da sind. Bei Bedarf auffüllen.',40,20,'figures/aufgabe_wechselsachen_640.jpg','Kinder'),(6,'Wechselsachen in der Schule auffüllen','alle zwei Wochen','Nachschauen, ob in der Schule noch genügend, dem aktuellen Wetter entsprechende Wechselsachen da sind. Bei Bedarf auffüllen.',40,20,'figures/aufgabe_wechselsachen_640.jpg','Kinder');
+INSERT INTO `aufgaben` VALUES (1,'Wäsche waschen','alle zwei Tage','Wäsche sortieren, Waschmaschine befüllen, Waschmittel rein und starten',10,15,'figures/aufgabe_waescheWaschen_640.jpg','Wäsche'),(2,'Wäsche trocknen','alle zwei Tage','Wäsche aus der Waschmaschine holen und aufhängen oder im Trockner trocknen',20,30,'figures/aufgabe_waescheTrocknen_640.jpg','Wäsche'),(3,'Wäsche legen','alle zwei Tage','trockene Wäsche legen und in den passenden Schrank einräumen',20,30,'figures/aufgabe_waescheLegen_640.jpg','Wäsche'),(4,'Boden wischen','einmal pro Woche','in allen Zimmer, die es nötig haben, den Boden nass wischen',100,45,'figures/aufgabe_bodenWischen_640.jpg','Putzen'),(5,'Wechselsachen und Windeln in der Kita auffüllen','alle zwei Wochen','Nachschauen, ob in der Kita noch genug Wechselsachen und Windeln da sind, bei Bedarf auffüllen.',40,20,'figures/aufgabe_auffuellenwindeln_640.jpg','Kinder'),(6,'Wechselsachen in der Schule auffüllen','alle zwei Wochen','Nachschauen, ob in der Schule noch genug Wechselsachen da sind, bei Bedarf auffüllen.',40,20,'figures/aufgabe_wechselsachen_640.jpg','Kinder'),(7,'Papiermüll wegschaffen','alle zwei Wochen','Papiermüll zu Hause sammeln und ins die großen Papiertonnen oder zum Wertstoffhof schaffen',100,60,'figures/aufgabe_papiermuell_640.jpg','Abfall'),(8,'Pfandflaschen wegschaffen','alle zwei Wochen','Pfandflaschen (Einweg, Mehrweg) zu Hause sammeln und wegschaffen',100,60,'figures/aufgabe_pfandwegschaffen_640.jpg','Abfall'),(9,'Reifenwechsel','alle sechs Monate','Um Ostern Sommereifen, im Oktober Winterreifen aufs Auto machen.',200,120,'figures/aufgabe_reifenwechsel_640.jpg','Auto'),(10,'Durchsicht','einmal im Jahr','Durchsicht/Hauptuntersuchung im Autohaus machen lassen',200,120,'figures/aufgabe_durchsicht_640.jpg','Auto');
 /*!40000 ALTER TABLE `aufgaben` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -80,4 +80,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-11-27 21:36:33
+-- Dump completed on 2024-11-29 17:44:41
