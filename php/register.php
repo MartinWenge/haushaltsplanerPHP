50705ae6aabff5e9daaefd00ea6da27d969d85a6
MartinWenge <wengenmayr.martin@outlook.de>
2024-12-07 21:26:10 +0100

    fix password handling in registration form

--- php/register.php
+++ php/register.php
@@ -17,7 +17,7 @@
         $accountData = getPasswortByUsername($_POST['usernameNew']);
 
         if($accountData['userId'] == NULL) {
-            if(createNeuenAccount($_POST['usernameNew'], $accountData['passwort'], $_POST['emailNew'], $_POST['birthdayNew'])) {
+            if(createNeuenAccount($_POST['usernameNew'], $_POST['passwordNew'], $_POST['emailNew'], $_POST['birthdayNew'])) {
                 header("Location: ../login.php?newAccount=" . $_POST['usernameNew']);
             } else {
                 echo "Fehler beim erstellen des neuen Nutzers, probiere es noch einmal.";
